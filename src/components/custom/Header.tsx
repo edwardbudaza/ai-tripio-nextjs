214c4fca83eb71e1e8b130bb708e786c4225ae90
edwardbudaza <edward.budaza@yahoo.co.za>
2024-08-10 22:40:11 +0200

    landing page

---
+++ src/components/custom/Header.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { FC } from "react";
+import Link from "next/link";
+import { UserButton, SignInButton, useUser } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import Image from "next/image";
+
+const Header: FC = () => {
+  const { user } = useUser();
+
+  return (
+    <div className="flex p-3 shadow-sm justify-between items-center px-5">
+      <Link href="/" passHref>
+        <Image src="/logo.svg" width={150} height={150} style={{ cursor: "pointer" }} alt="Logo" />
+      </Link>
+      <div>
+        {user ? (
+          <div className="flex items-center gap-3">
+            <Link href="/create-trip" passHref>
+              <Button variant="outline" className="rounded-full">
+                + Create Trips
+              </Button>
+            </Link>
+            <Link href="/my-trips" passHref>
+              <Button variant="outline" className="rounded-full">
+                My Trips
+              </Button>
+            </Link>
+            <UserButton />
+          </div>
+        ) : (
+          <SignInButton mode="modal">
+            <Button>Sign In</Button>
+          </SignInButton>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/custom/Hero.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "../ui/button";
+
+function Hero() {
+  return (
+    <div className="flex flex-col items-center mx-56 gap-9">
+        <h1 className="font-extrabold text-[50px] text-center mt-16">
+            <span className="bg-gradient-to-bl from-yellow-500 via-purple-500 to-blue-500 inline-block text-transparent bg-clip-text">  
+                Unlock Your Dream Journey with AI: 
+            </span>
+            Cusom Plans in Seconds
+        </h1>
+        <p className="text-xl text-gray-500 text-center">
+            Your ultimate travel curator and trip planner, crafting bespoke itineraries perfectly aligned with your interests and budget.
+        </p>
+
+        <Link href={"/create-trip"} passHref>
+            <Button>Get Started, It's Free</Button>
+        </Link>
+        
+        <Image
+            src="/landing.png"
+            alt="Cover" 
+            width={800}
+            height={800}
+            className="mt-5"
+        />
+    </div>
+  );
+};
+export default Hero;
